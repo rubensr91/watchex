6281669cbe2167620dbb6f50dd20295afbfb6806
Carlos Navas <carlosnavas@BCNCARNAVAMAC.local>
2019-09-26 12:57:17 +0200

    first commit

---
+++ lib/watchexs.ex
@@ -0,0 +1,18 @@
+defmodule Watchexs do
+  @moduledoc """
+  Documentation for Watchexs.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Watchexs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/watchexs_test.exs
@@ -0,0 +1,8 @@
+defmodule WatchexsTest do
+  use ExUnit.Case
+  doctest Watchexs
+
+  test "greets the world" do
+    assert Watchexs.hello() == :world
+  end
+end
